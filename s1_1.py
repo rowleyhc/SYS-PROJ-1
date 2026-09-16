41a8ff231c369703b54a908dd369fef9d65592d2
rowleyhc <hrowley@terpmail.umd.edu>
2026-09-16 15:23:52 -0400

    Create s1_1.py
    Section 1.1 stage mass solver function, next step is to set up calling of this function to sweep through various propellant combinations and stage 1 delta-v values

---
+++ s1_1.py
@@ -0,0 +1,59 @@
+import math
+
+#FUNCTION calculate_stage_mass():
+#Date Created: 9/16/2026
+#Author: Henry Rowley
+#Note: isp_1 MUST be sea level isp, isp_2 MUST be vacuum isp
+
+
+def calculate_stage_masses(
+    delta_v_1: float, #specified delta-v for first stage (m/s)
+    delta_1: float, #inert mass fraction of stage 1
+    delta_2: float, #inert mass fraction of stage2
+    isp_1: float, #stage 1 isp(sea level) (sec)
+    isp_2: float, #stage 2 isp(vacuum) (sec)
+    delta_v_total: float = 12300.0, #total delta v, m/s, M1 NOTE: Might want to update dvtot, mpl, and g0 from being defined here to accessing a shared file where we store all constants
+    m_pl: float = 26000.0, #payload mass, M2
+    g0:float = 9.8 #g0 (m/s^2) NOTE: lecture provides 9.8 but using higher precision value here might be desirable
+):
+    delta_v_2 = delta_v_total - delta_v_1 
+
+    #calculate exhaust velocity of each stage
+    Ve_1 = g0 * isp_1
+    Ve_2 = g0 * isp_2
+
+    #calculate mass ratios using alternate form of rocket equation
+    r_1 = math.exp(-delta_v_1/Ve_1)
+    r_2 = math.exp(-delta_v_2/Ve_2)
+
+    #calculate payload fractions using parametric mass ratio r
+    lambda_1 = r_1 - delta_1
+    lambda_2 = r_2 - delta_2
+
+    #check feasibility and pass error if applicable
+    error = [lambda_1 <= 0, lambda_2 <=0] #note error now list of two booleans, gets output and can be used when sweeping through delta_v_1 values to know which values are invalid
+    if any(error): #if either entry in error is true
+        m_0 = m_0_2 = m_in_1 = m_in_2 = m_pr_1 = m_pr_2 = 0.0 
+
+    else: #otherwise, calculate masses
+        #calculate initial mass of each stage using payload fraction definition
+        m_0_2 = m_pl/lambda_2 
+        m_0 = m_0_2/lambda_1 #treat m_0_2 as m_pl for stage 1 NOTE: m_0 total launch vehicle mass, equivalently m_0_1
+
+        #calculate inert mass of each stage
+        m_in_1 = delta_1 * m_0
+        m_in_2 = delta_2 * m_0_2
+
+        #calculate propellant mass of each stage
+        m_pr_1 = m_0 - m_in_1 - m_0_2 #treating m_0_2 as m_pl for stage 1
+        m_pr_2 = m_0_2 - m_in_2 - m_pl
+
+    return {
+        "m_0": m_0,
+        "m_in_1": m_in_1,
+        "m_pr_1": m_pr_1,
+        "m_0_2": m_0_2,
+        "m_in_2": m_in_2,
+        "m_pr_2": m_pr_2,
+        "Error": error,
+    }
