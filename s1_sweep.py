dddb11a4ed57a9810c37bfe7d6f645a3adcc95e8
Jacob Harmon <106879881+BILLLYGOAT@users.noreply.github.com>
2026-09-16 21:40:27 -0400

    Add sweep_delta_v function for propellant analysis

---
+++ s1_sweep.py
@@ -0,0 +1,54 @@
+
+# Author: Jacob Harmon
+# Date: 9/16/2026
+
+import math
+
+from s1_1 import calculate_stage_masses
+
+
+def sweep_delta_v(
+    propellant1: dict,
+    propellant2: dict,
+    delta_v_total: float,
+    delta_v1_min: float,
+    m_pl: float,
+    g0: float,
+    dv_step: float
+):
+
+    results = []
+    dv1 = delta_v1_min
+    while(dv1 < delta_v_total):
+        sweep = calculate_stage_masses(
+            delta_v_1=dv1,
+            delta_1=propellant1["inert_mass_fraction"],
+            delta_2=propellant2["inert_mass_fraction"],
+            isp_1=propellant1["isp_sea_level_s"],
+            isp_2=propellant2["isp_vacuum_s"],
+            delta_v_total=delta_v_total,
+            m_pl=m_pl,
+            g0=g0,
+        )
+
+        data = {
+            "delta_v_1": dv1,
+            "delta_v_2": delta_v_total - dv1,
+            "m_0": sweep["m_0"],
+            "m_in_1": sweep["m_in_1"],
+            "m_pr_1": sweep["m_pr_1"],
+            "m_0_2": sweep["m_0_2"],
+            "m_in_2": sweep["m_in_2"],
+            "m_pr_2": sweep["m_pr_2"],
+            "Error": sweep["Error"],
+        }
+
+        results.append(data)
+        dv1 += dv_step
+
+    return results
+ 
+
+
+
+    
